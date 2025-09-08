e1e36488318ed270e694f94b4e28a0bf053d75a3
Rushclin <takamrushclin@gmail.com>
2025-09-08 19:02:25 +0100

    adding layout

---
+++ src/layout/PublicLayout.tsx
@@ -0,0 +1,35 @@
+import LanguageSwitcher from "@/components/LanguageSwitcher";
+import { appConfig } from "@/config/app";
+import Head from "next/head";
+import React from "react";
+
+interface PublicLayoutProps {
+  children: React.ReactNode;
+  title?: string;
+  description?: string;
+}
+
+const PublicLayout: React.FC<PublicLayoutProps> = ({
+  children,
+  description,
+  title,
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{title || appConfig.name}</title>
+        <meta
+          property="description"
+          content={description || appConfig.description}
+        />
+        <meta property="og:title" content={title} />
+      </Head>
+      <div>
+        {children}
+        <LanguageSwitcher className="bottom-5" />
+      </div>
+    </div>
+  );
+};
+
+export default PublicLayout;
