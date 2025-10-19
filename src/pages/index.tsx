5d1ea006803ada543c7aedabf30e515c586b5dc0
Rushclin <takamrushclin@gmail.com>
2025-10-19 10:16:44 +0100

    update translate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,10 @@ import PublicLayout from "@/layout/public/PublicLayout";
 import { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
+  const {t} = useTranslation("home")
   const [mounted, setMounted] = useState(false); // https://stackoverflow.com/questions/73162551/how-to-solve-react-hydration-error-in-nextjs
 
   useEffect(() => {
@@ -19,7 +21,7 @@ const HomePage = () => {
   if (!mounted) return <Loading />;
 
   return (
-    <PublicLayout title="Home">
+    <PublicLayout title={t("title")}>
       <Header />
       <Hero />
       <FormGeneration />
